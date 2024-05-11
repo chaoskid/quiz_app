267c226198f66d44c1ecf2614ab5fddbbbf47281
Vaisakh <vaisakhpariyacheri@gmail.com>
2024-05-11 20:57:03 +1000

    Create quiz html page tags corrected

--- question_form.py
+++ question_form.py
@@ -4,11 +4,11 @@ from wtforms.validators import DataRequired
 from wtforms.widgets import RadioInput
 
 class OptionForm(FlaskForm):
-    text = StringField('Option', validators=[DataRequired()])
-    is_correct = BooleanField('Is Correct')
+    text = StringField('Option:', validators=[DataRequired()])
+    is_correct = BooleanField('Is Correct:')
 
 class QuestionForm(FlaskForm):
-    text = StringField('Question', validators=[DataRequired()])
+    text = StringField('Question:', validators=[DataRequired()])
     options = FieldList(FormField(OptionForm), min_entries=4, max_entries=4)
 
 class QuizForm(FlaskForm):
